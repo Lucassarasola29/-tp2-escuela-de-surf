73e8ff74f82888d2f89537875e5427eae9eb077d
LucianoDindart <lucianodindart25@gmail.com>
2026-09-20 21:18:04 -0300

    Nuevo formato de fecha

--- src/Inscripcion.java
+++ src/Inscripcion.java
@@ -1,13 +1,18 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Inscripcion {
     private Turno turno;
     private String fecha;
     private Tabla tabla;
     private boolean asistencia;
 
-    public Inscripcion(Turno turno, String fecha) {
+    public Inscripcion(Turno turno) {
         this.turno = turno;
-        this.fecha = fecha;
         this.asistencia = false;
+        DateFormat fecha = new SimpleDateFormat("dd/MM/yyyy");
+        this.fecha = fecha.format(new Date());
     }
 
     public void setTabla(Tabla tabla) {
@@ -29,6 +34,7 @@ public class Inscripcion {
     public void setAsistencia(boolean asistencia){
         this.asistencia = asistencia;
     }
+
 
 }
 
