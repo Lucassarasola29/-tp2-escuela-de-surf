ebb3be3bdaede723a2314a57174282540ec28035
Cacutss <cuentasmurfdeleague@gmail.com>
2026-09-21 18:52:57 -0300

    arreglado inicializar arreglo en constructor

--- Instructor.java
+++ Instructor.java
@@ -1,57 +1,52 @@
 public class Instructor {
-    private String nombre; 
-    private String apellido; 
+    private String nombre;
+    private String apellido;
     private int numeroMatricula;
-    private Turno turnos[]; 
+    private Turno turnos[];
     private int cantTurnos;
-    
-    public Instructor (String nombre, String apellido, int numeroMatricula) {
+
+    public Instructor(String nombre, String apellido, int numeroMatricula) {
         this.nombre = nombre;
-        this.apellido = apellido; 
-        this.numeroMatricula = numeroMatricula; 
+        this.apellido = apellido;
+        this.numeroMatricula = numeroMatricula;
         this.cantTurnos = 0;
-
-        
+        this.turnos = new Turno[10];
 
     }
-    public String getNombre (){
-        return this.nombre; 
+
+    public String getNombre() {
+        return this.nombre;
     }
 
-    public String getApellido (){
+    public String getApellido() {
         return this.apellido;
     }
 
     public int getnumeroMatricula() {
-        return this.numeroMatricula; 
+        return this.numeroMatricula;
     }
 
-    
-    public int getCantTurnos () {
+    public int getCantTurnos() {
         return cantTurnos;
     }
 
-    public void agregarTurno (Turno turno){
+    public void agregarTurno(Turno turno) {
         if (cantTurnos < turnos.length) {
-            this.turnos [cantTurnos] = turno;
-             this.cantTurnos++;
+            this.turnos[cantTurnos] = turno;
+            this.cantTurnos++;
         } else {
-            System.out.println("Error: el instructor " + apellido + "no se puede tener mas de " + turnos.length + "turnos asignados");
+            System.out.println("Error: el instructor " + apellido + "no se puede tener mas de " + turnos.length
+                    + "turnos asignados");
         }
-        
+
     }
-    
-    public Turno[] getTurnos(){
+
+    public Turno[] getTurnos() {
         return turnos;
     }
 
-    
-    public String descripcion(){
-        return "Instructor: " + nombre + "" + apellido + "" + " | Matricula: " + numeroMatricula; 
+    public String descripcion() {
+        return "Instructor: " + nombre + "" + apellido + "" + " | Matricula: " + numeroMatricula;
     }
-
-
-
-
 
 }
