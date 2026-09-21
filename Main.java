ca36b2f37e95f51d03bc7c186e31ed9886a32e3d
Cacutss <cuentasmurfdeleague@gmail.com>
2026-09-21 18:18:55 -0300

    movido todo de src a root

--- Main.java
+++
@@ -1,7 +0,0 @@
-import src.Escuela;
-
-public class Main {
-	public void main(String args[]) {
-		Escuela escuela = new Escuela("pepino", "batata");
-	}
-}
--- src/Tabla.java
+++ src/Tabla.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Tabla {
 	private String codigo;
 	private double largoEnPies;
