1402fede5e42aec5ae69e4f4c85da60c5d57dedd
Lucassarasola29 <lucassarasola29@gmail.com>
2026-09-20 22:27:47 -0300

    Actualizado instructor

---
+++ src/Instructor.java
@@ -0,0 +1,51 @@
+public class Instructor {
+    private String nombre; 
+    private String apellido; 
+    private int numeroMatricula;
+    private Turno turnos[]; 
+    private int cantTurnos;
+    
+    public Instructor (String nombre, String apellido, int numeroMatricula) {
+        this.nombre = nombre;
+        this.apellido = apellido; 
+        this.numeroMatricula = numeroMatricula; 
+        this.cantTurnos = 0;
+        
+
+    }
+    public String getNombre (){
+        return this.nombre; 
+    }
+
+    public String getApellido (){
+        return this.apellido;
+    }
+
+    public int getnumeroMatricula() {
+        return this.numeroMatricula; 
+    }
+    
+    public int getCantTurnos () {
+        return cantTurnos;
+    }
+
+    public void agregarTurno (Turno turno){
+        if (cantTurnos < turnos.length) {
+            this.turnos [cantTurnos] = turno;
+             this.cantTurnos++;
+        } else {
+            System.out.println("Error: el instructor " + apellido + "no se puede tener mas de " + turnos.length + "turnos asignados");
+        }
+        
+    }
+    
+    public Turno[] getTurnos(){
+        return turnos;
+    }
+
+    
+    public String descripcion(){
+        return "Instructor: " + nombre + "" + apellido + "" + " | Matricula: " + numeroMatricula; 
+    }
+
+}
