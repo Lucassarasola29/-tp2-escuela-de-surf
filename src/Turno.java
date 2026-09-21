ff05616f5002528b3dc60631368e64ed6ff157e7
Lucassarasola29 <lucassarasola29@gmail.com>
2026-09-21 17:50:36 -0300

    Actualizado

--- src/Turno.java
+++ src/Turno.java
@@ -4,18 +4,14 @@ public class Turno {
     private String horario; 
     private Instructor instructor;
     private int cupo; 
-    private Tabla tablas[];
     private Inscripcion[] inscripciones;
-    private int cantTablas;
     private int cantInscripciones;
     
     public Turno (String fecha, String horario) {
         this.fecha = fecha; 
         this.horario = horario;
         this.cupo = 0; 
-        this.tablas = new Tabla [CUPO_MAXIMO];
         this.inscripciones = new Inscripcion [CUPO_MAXIMO];
-        this.cantTablas = 0;
         this.cantInscripciones = 0;
   
 
@@ -38,24 +34,12 @@ public class Turno {
     public Instructor getInstructor () {
         return instructor;
     }
-    public int getCantTablas (){
-        return cantTablas;
-    }
-
+ 
     public int getCantInscripciones (){
-        return cantInscripcione; 
+        return cantInscripciones; 
     }
 
-    public void agregarTabla(Tabla tabla) {
-        if (tabla == null) {
-            System.out.println("Error: No se puede agregar una tabla nula.");
-        } else if (cantTablas < tablas.length) {
-            tablas[cantTablas] = tabla;
-            cantTablas++;
-        } else {
-            System.out.println("Error: Se alcanzó el límite máximo de tablas asignadas al turno.");
-        }
-    }
+ 
 
     public void agregarInscripcion(Inscripcion inscripcion) {
         if (inscripcion == null) {
@@ -65,6 +49,7 @@ public class Turno {
             cantInscripciones++;
         } else {
             System.out.println("Error: El turno no tiene cupo disponible para más inscripciones.");
+
         }
     }
 
@@ -77,6 +62,23 @@ public class Turno {
             System.out.println("Error, no se puede asginar un instructor nulo al turno");
     
         }
+    }
+
+    public boolean tablaDisponible (Tabla tabla){
+        if (tabla == null ) {
+            return false; 
+        }
+        for (int i = 0; i < cantInscripciones; i++) {
+            Tabla tablaOcupada = inscripciones[i].getTabla();
+
+            if (tablaOcupada != null && tablaOcupada.equals(tabla)){
+                return false; 
+            }
+
+            
+            
+        }
+        return true;
     }
 
     public String descripcion () {
