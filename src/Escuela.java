2b72930fab81dcfe56395614baa09c20b8d8bda3
Cacutss <cuentasmurfdeleague@gmail.com>
2026-09-21 01:04:14 -0300

    esqueleto de escuela

---
+++ src/Escuela.java
@@ -0,0 +1,67 @@
+public class Escuela {
+	private static final int MAX_TABLAS = 100;
+	private static final int MAX_TURNOS = 100;
+	private static final int MAX_ALUMNOS = 100;
+	private static int cantAlumnos = 0;
+
+	private String nombre;
+	private String playa;
+	private int cantTurnos;
+	private Turno turnos[];
+	private int cantTablas;
+	private Tabla tablas[];
+	private Alumno alumnos[];
+
+	public Escuela(String nombre, String playa) {
+		this.nombre = nombre;
+		this.playa = playa;
+		this.turnos = new Turno[MAX_TURNOS];
+		this.tablas = new Tabla[MAX_TABLAS];
+		this.alumnos = new Alumno[MAX_ALUMNOS];
+	}
+
+	public String descripcion() {
+		return "Escuela: " + this.nombre + "Playa: " + this.playa + "Cantidad turnos: " + this.cantTurnos
+				+ "Cantidad tablas: " + this.cantTablas;
+	}
+
+	public void crearTurno(String fecha, String horario) {
+		this.turnos[cantTurnos] = new Turno(fecha, horario);
+		this.cantTurnos++;
+	}
+
+	public void asignarTurno(Instructor instructor) {
+		if (instructor != null) {
+			for (Turno turno : turnos) {
+				if (turno.getInstructor() == null) {
+					instructor.agregarTurno(turno);
+					return;
+				}
+			}
+		} else {
+			System.out.println("No se puede agregar turno a instructor nulo");
+		}
+	}
+
+	public void repartirTablas() {
+
+	}
+
+	public void registrarAlumno(Alumno alumno) {
+		if (alumno != null) {
+			this.alumnos[cantAlumnos] = alumno;
+			this.cantAlumnos++;
+		} else {
+			System.out.println("No se puede añadir un alumno nulo");
+		}
+	}
+
+	public void cargarTabla(Tabla tabla) {
+		if (tabla != null) {
+			this.tablas[cantTablas] = tabla;
+			this.cantTablas++;
+		} else {
+			System.out.println("No se puede añadir una tabla nula");
+		}
+	}
+}
